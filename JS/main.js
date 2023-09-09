d70ec5eeb3b549369d7635cd4fb49fdeb3ee40d4
Deyon Bur <41640270+Kammad@users.noreply.github.com>
2023-09-09 13:29:59 -0500

    Changes

--- JS/main.js
+++ JS/main.js
@@ -75,4 +75,10 @@ $(function () {
   }
 
   initMap();
+
+  $('.burger').on('click', function (e) {
+    e.preventDefault()
+    $('.header__top').toggleClass('header__top--open')
+    $('.burger').toggleClass('burger--open')
+  })
 })
